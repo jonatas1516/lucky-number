cb5f914ebefec59e9a0eae6f089caa8ca156f5a0
jonatas1516 <jonatas1516@gmail.com>
2023-12-03 00:17:55 -0300

    update js Else if

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,29 +11,23 @@ btn.addEventListener("click", drawer);
 function drawer() {
   if (minFrom.value == "") {
     alert("Digite um valor mínimo!");
-  } else {
-    if (maxTo.value == "") {
-      alert("Digite um valor máximo!");
+  } else if (maxTo.value == "") {
+    alert("Digite um valor máximo!");
+  } else if (minFrom.value == maxTo.value) {
+    if (minFrom.value == "" && maxTo.value == "") {
+      alert("Os valores não foram digitados!");
     } else {
-      if (minFrom.value == maxTo.value) {
-        if (minFrom.value == "" && maxTo.value == "") {
-          alert("Os valores não foram digitados!");
-        } else {
-          alert("Os valores são iguais!");
-        }
-      } else {
-        if (minFrom.value > maxTo.value) {
-          alert("O valor mínimo não pode ser maior que o valor máximo!");
-        } else {
-          const min = Math.ceil(minFrom.value);
-
-          const max = Math.floor(maxTo.value);
-
-          const result = Math.floor(Math.random() * (max - min + 1)) + min;
-
-          text.innerHTML = result;
-        }
-      }
+      alert("Os valores são iguais!");
     }
+  } else if (minFrom.value > maxTo.value) {
+    alert("O valor mínimo não pode ser maior que o valor máximo!");
+  } else {
+    const min = Math.ceil(minFrom.value);
+
+    const max = Math.floor(maxTo.value);
+
+    const result = Math.floor(Math.random() * (max - min + 1)) + min;
+
+    text.innerHTML = result;
   }
 }
