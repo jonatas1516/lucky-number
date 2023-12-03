3618f45e212b72496fa52ea21eb2c76147d91121
jonatas1516 <jonatas1516@gmail.com>
2023-12-02 23:13:20 -0300

    update js comparison of values

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,11 +9,31 @@ const btn = document.querySelector(".btn");
 btn.addEventListener("click", drawer);
 
 function drawer() {
-  const min = Math.ceil(minFrom.value);
+  if (minFrom.value == "") {
+    alert("Digite um valor mínimo!");
+  } else {
+    if (maxTo.value == "") {
+      alert("Digite um valor máximo!");
+    } else {
+      if (minFrom.value == maxTo.value) {
+        if (minFrom.value == "" && maxTo.value == "") {
+          alert("Os valores não foram digitados!");
+        } else {
+          alert("Os valores são iguais!");
+        }
+      } else {
+        if (minFrom.value > maxTo.value) {
+          alert("O valor mínimo não pode ser maior que o valor máximo!");
+        } else {
+          const min = Math.ceil(minFrom.value);
 
-  const max = Math.floor(maxTo.value);
+          const max = Math.floor(maxTo.value);
 
-  const result = Math.floor(Math.random() * (max - min + 1)) + min;
+          const result = Math.floor(Math.random() * (max - min + 1)) + min;
 
-  text.innerHTML = result;
+          text.innerHTML = result;
+        }
+      }
+    }
+  }
 }
